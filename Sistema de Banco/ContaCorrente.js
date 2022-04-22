f32b01e8946754adb27ebab3fab0d0751a19ab2f
*Wesley <wesleydaybusi12@gmail.com>
2022-04-22 15:13:15 -0300

    Finalizando essa etapa do curso, aprendendo sobre static

--- Sistema de Banco/ContaCorrente.js
+++ Sistema de Banco/ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
+  static numeroDeContas = 0;
   agencia;
   _cliente;
   _saldo = 0;
@@ -22,6 +23,7 @@ export class ContaCorrente{
   constructor(agencia, cliente){
     this.cliente = cliente;
     this.agencia = agencia;
+    ContaCorrente.numeroDeContas += 1;
   }
 
   sacar(valor){
